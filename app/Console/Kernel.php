1e42b94c29635d85dacedc93c42550dfb09d48ac
raoasifraza11 <raoasifraza1@hotmail.com>
2017-10-03 14:47:24 +0500

    Create custom command app install
    Created the once command for installation of application

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
-        Commands\setuplocally::class
+        Commands\setuplocally::class,
+        Commands\App::class
     ];
 
     /**
