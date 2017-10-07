6cffae830d239c3bcc74daf206c9703604d164e1
raoasifraza11 <raoasifraza1@hotmail.com>
2017-10-07 15:09:23 +0500

    Mail overiden funciton

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class CoreController extends Controller
 {
-    public function sendMail($type, $path, $data, $message, $to, $from, $subject){
+    public static function sendMail($type = 'html', $template = 'email.welcome', $data_array = array('name'=>"Ani Motors"), $to, $subject){
 
+        \Mail::send([$type => $template ], $data_array, function($message) use (&$to, &$subject)  {
+            // Set the receiver and subject of the mail.
+            $message->to($to)
+                ->subject($subject)
+                ->from(__('config.send_from'), 'We Care About You!');
+        });
     }
 }
