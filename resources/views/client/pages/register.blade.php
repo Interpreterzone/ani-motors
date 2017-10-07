c4386e5383e74912f49fb1ba19137c4f30d773e4
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-10-07 02:51:09 -0700

    signup form completed

--- resources/views/client/pages/register.blade.php
+++ resources/views/client/pages/register.blade.php
@@ -7,43 +7,37 @@
                     <h2 class="login-signup__heading">Create your Ani Motors account</h2>
                 </div>
                 <div class="panel-body sign-up">
-<<<<<<< HEAD
                     <form class="new_client" id="new_client"  action="{{ url('register') }}" method="POST"  accept-charset="UTF-8" >
                         {{ csrf_field() }}
-=======
-                    <form action="{{ url('client/signup') }}" method="POST">
-                  <!-- <form class="new_client" id="new_client"  action="{{ url('client/signup') }}" method="POST"  accept-charset="UTF-8" ><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="ozhXmXfuKx4oeVaJIwZcCk15BqKradWKprrgz8D063bq+dFfzYqH1adkHqxgMY+Gl8lkeQUfIW/OQTBfaEJfKA==">
-                      -->
->>>>>>> 5d666de9e6b5d88e22091f741f0610504a6bd29e
                         <div class="row name-row">
                             <div class="col-sm-6">
                                 <div class="form-group has-feedback">
-                                    <input autofocus="autofocus" placeholder="First Name" required="required" class="form-control text-field-box" id="first_name" type="text" name="client[first_name]">
+                                    <input autofocus="autofocus" placeholder="First Name" required="required" class="form-control text-field-box" id="first_name" type="text" name="first_name">
                                    </div>
                             </div>
 
                             <div class="col-sm-6">
                                 <div class="form-group has-feedback">
-                                    <input autofocus="autofocus" required="required"  placeholder="Last Name" class="form-control text-field-box" id="last_name" type="text" name="client[last_name]">
+                                    <input autofocus="autofocus" required="required"  placeholder="Last Name" class="form-control text-field-box" id="last_name" type="text" name="last_name">
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group has-feedback">
-                            <input autofocus="autofocus" required="required" placeholder="Phone" class="form-control text-field-box" id="phone" type="text" name="client[phone]">
+                            <input autofocus="autofocus" required="required" placeholder="Phone" class="form-control text-field-box" id="phone" type="text" name="phone">
                         </div>
 
                         <div class="form-group has-feedback">
-                            <input autofocus="autofocus" required="required" placeholder="Email" class="form-control text-field-box" id="email" type="email" value="" name="client[email]">
+                            <input autofocus="autofocus" required="required" placeholder="Email" class="form-control text-field-box" id="email" type="email" value="" name="email">
                         </div>
 
                         <div class="form-group has-feedback">
-                            <input autocomplete="off" required="required" placeholder="Password" class="form-control text-field-box" id="password" pattern=".{8,}" oninvalid="setCustomValidity('Password must be at least 8 characters long')" onchange="try{setCustomValidity('')}catch(e){}" type="password" name="client[password]">
+                            <input autocomplete="off" required="required" placeholder="Password" class="form-control text-field-box" id="password" pattern=".{8,}" oninvalid="setCustomValidity('Password must be at least 8 characters long')" onchange="try{setCustomValidity('')}catch(e){}" type="password" name="password">
                         </div>
 
                         <div class="form-group">
                             <label for="client_first_name">How did you hear about us?</label>
-                            <select required="required" class="form-control text-field-box" name="client[how_did_you_hear_about_us]" id="client_how_did_you_hear_about_us"><option value="">Please select...</option>
+                            <select required="required" class="form-control text-field-box" name="how_did_you_hear_about_us" id="client_how_did_you_hear_about_us"><option value="">Please select...</option>
                                 <option value="Radio">Radio</option>
                                 <option value="Uber Marketplace">Uber Marketplace</option>
                                 <option value="Facebook group">Facebook group</option>
@@ -55,12 +49,12 @@
                         </div>
 
                         <div class="form-group has-feedback">
-                            <input autocomplete="off" class="form-control text-field-box" placeholder="Referral code" id="referral_code" type="text" name="client[referred_by_code]">
+                            <input autocomplete="off" class="form-control text-field-box" placeholder="Referral code" id="referral_code" type="text" name="referred_by_code">
                         </div>
 
                         <div class="login-signup__bottom">
                             <div class="checkbox">
-                                <input name="client[terms]" type="hidden" value="0"><input required="required" type="checkbox" value="1" name="client[terms]" id="client_terms">
+                                <input name="terms" type="hidden" value="0"><input required="required" type="checkbox" value="1" name="client[terms]" id="client_terms">
                                 <label for="client_terms">I agree with Drover's <a href="#" target="_blank">Terms of Service</a></label>
                             </div>
                         </div>
