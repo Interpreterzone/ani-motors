05d2d75e2802701c3264b4592dea2ca0ec00ba7a
hariskhurshid <32355482+hariskhurshid@users.noreply.github.com>
2017-10-13 03:19:11 -0700

    mail confirmation
    mail confirmation  template

---
+++ confirm.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+
+    <link href="{{ URL::asset('client\css\font-awesome.min.css') }}" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <!-- mdb-style -->
+    <link href="{{ URL::asset('email\css\bootstrap-mdb-custom.css') }}" rel="stylesheet" />
+    <!-- mdb-style -->
+
+
+</head>
+<body>
+<div class="page-container">
+    <div class="page-content">
+        <div class="page-content-wrap">
+<div class="card card-block">
+    <div class="card-content">
+        <div class="card-header badge-lime">
+            <div class="card-heading ">
+                <h2 class="card-title ">Ani Motors <span class="heading badge-amber"> Email Confirmation</span></h2>
+            </div>
+
+        </div>
+        <div class="card-body">
+            <div class="text-center">
+               <div class="card">
+                   <h2>wellcome</h2>
+                   <div class="card-description">
+                       <h2>Dummy text</h2>
+                   </div>
+                   <div class="card-description">
+                       <h2>Dummy text</h2>
+                   </div>
+                   <div class="card-description">
+                       <h2>Dummy text</h2>
+                   </div>
+                   <div class="card-footer">
+                       <span>Your confirmation code is.. <strong>456</strong></span> <br>
+                       <span class=" btn-block btn-lg btn-info">
+
+                               <marquee>Email Confirmation
+                                   <span class="fa fa-arrow-circle-right "> &nbsp;
+                                       <a href="">Click Here</a>
+                                   </span>
+                               </marquee>
+
+                       </span>
+
+                   </div>
+               </div>
+            </div>
+
+
+        </div>
+        <div class="card-footer">
+           <div class="footer-copyright">
+               All right reserved Animotors 2017
+           </div>
+
+        </div>
+    </div>
+</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
